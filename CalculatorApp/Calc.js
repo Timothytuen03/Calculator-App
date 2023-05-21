6bd495b194302ec9501db55c6a79324a24ea01cf
Timothy Tuen <116755501+Timothytuen03@users.noreply.github.com>
2023-05-21 12:23:39 -0400

    Add files via upload

---
+++ CalculatorApp/Calc.js
@@ -0,0 +1,22 @@
+var num1 = ;
+var num2 = ;
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function mod(num1, num2) {
+    return num1 % num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
